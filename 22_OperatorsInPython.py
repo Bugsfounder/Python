4b277e3d4d2968f90f802250e2ad84652564b25c
Bugs Founder <bugsfounder2021@gmail.com>
2021-10-03 04:28:20 +0100

    working on functions and docstrings in python

---
+++ 22_OperatorsInPython.py
@@ -0,0 +1,72 @@
+# OPERATORS IN PYTHON
+# Arithmetic Operators
+# Assignment Operators
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membership Operators
+# Bitwise Operators
+
+# ARITHMETIC OPERATORS
+# print("5 + 6 is ", 5 + 6)  # + is a arithmetic operator
+# print("5 - 6 is ", 5 - 6)  # - is a arithmetic operator
+# print("5 * 6 is ", 5 * 6)  # * is a arithmetic operator
+# print("5 ** 6 is ", 5 ** 6)  # * is a arithmetic operator
+# print("5 / 6 is ", 5 / 6)  # / is a arithmetic operator
+# print("5 // 6 is ", 5 // 6)  # // is a arithmetic operator
+# print("5 % 6 is ", 5 % 6)  # % is a arithmetic operator
+
+# ASSIGNMENT OPERATORS
+# print("ASSIGNMENT OPERATORS")
+# x = 5  # = is a assignment operator
+# print(x)
+# x += 7  # += is a assignment operator
+# print(x)
+# x *= 7  # *= is a assignment operator
+# print(x)
+# x %= 7  # %= is a assignment operator
+# print(x)
+# x /= 7  # /= is a assignment operator
+# print(x)
+# x -= 7  # -= is a assignment operator
+# print(x)
+# x %= 7  # %= is a assignment operator
+# print(x)
+
+# COMPARISON OPERATORS
+# i = 8
+# print(i == 5)  # == is a comparison operator
+# print(i == 8)
+# print(i != 5)  # ! not operator is a comparison operator
+# print(i > 5)  # > greater than operator is a comparison operator
+# print(i < 5)  # < less than operator is a comparison operator
+# print(i <= 5)  # <= less than equal to operator is a comparison operator
+# print(i >= 5)  # >= greater than equal to operator is a comparison operator
+
+
+# LOGICAL OPERATORS
+# a = True
+# b = False
+# print(a and a)
+# print(a and b)
+# print(a or b)
+
+# IDENTITY OPERATORS
+# a = True
+# b = False
+# print(a is b)
+# print(a is not b)
+
+# MEMBERSHIP OPERATORS
+# lst = [32, 54, 12, 54, 23, 12, 17]
+# print(23 in lst)
+# print(23 not in lst)
+
+# BITWISE OPERATOR
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+
+print(0 & 2)
+print(0 | 3)
---
+++ 23_ShortHandIfElseNotationInPython.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a : "))
+b = int(input("Enter b: "))
+
+# if a > b: print("a is greater than b")
+
+print("b a se bara hai bhai") if (a < b) else print("a b se bara hai bhai")
